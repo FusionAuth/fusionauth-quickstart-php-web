d254a5063e49516e2474474dba1b5021faaf2f9e
Richard JE Cooke <RichardJECooke@pm.me>
2023-11-16 15:05:01 +0200

    get env variables

--- your-application/src/auth.php
+++ your-application/src/auth.php
@@ -1,62 +1,62 @@
 <?php
 
-require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php'; # load all composer packages automatically. root folder is one level up.
 
-// $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-// $dotenv->load();
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
+$dotenv->load();
 
-// $fusionAuthClientId = $_ENV['FUSIONAUTH_CLIENT_ID'] ?? getenv('FUSIONAUTH_CLIENT_ID');
-// $fusionAuthClientSecret = $_ENV['FUSIONAUTH_CLIENT_SECRET'] ?? getenv('FUSIONAUTH_CLIENT_SECRET');
-// $fusionAuthBaseUrl = $_ENV['FUSIONAUTH_BASE_URL'] ?? getenv('FUSIONAUTH_BASE_URL');
-// $fusionAuthRedirectUrl = $_ENV['FUSIONAUTH_REDIRECT_URL'] ?? getenv('FUSIONAUTH_REDIRECT_URL');
+$fusionAuthClientId =     $_ENV['FUSIONAUTH_CLIENT_ID']     ?? getenv('FUSIONAUTH_CLIENT_ID');
+$fusionAuthClientSecret = $_ENV['FUSIONAUTH_CLIENT_SECRET'] ?? getenv('FUSIONAUTH_CLIENT_SECRET');
+$fusionAuthBaseUrl =      $_ENV['FUSIONAUTH_BASE_URL']      ?? getenv('FUSIONAUTH_BASE_URL');
+$fusionAuthRedirectUrl =  $_ENV['FUSIONAUTH_REDIRECT_URL']  ?? getenv('FUSIONAUTH_REDIRECT_URL');
 
 
 
-$provider = new \JerryHopper\OAuth2\Client\Provider\FusionAuth([
-    'clientId'          => '{client-id}',
-    'clientSecret'      => '{client-secret}',
-    'redirectUri'       => 'https://example.com/callback-url',
-    'urlAuthorize'            => 'fusionauth:9011/oauth2/authorize',
-    'urlAccessToken'          => 'fusionauth:9011/oauth2/token',
-    'urlResourceOwnerDetails' => 'fusionauth:9011/oauth2/userinfo',
-]);
+// $provider = new \JerryHopper\OAuth2\Client\Provider\FusionAuth([
+//     'clientId'          => '{client-id}',
+//     'clientSecret'      => '{client-secret}',
+//     'redirectUri'       => 'https://example.com/callback-url',
+//     'urlAuthorize'            => 'fusionauth:9011/oauth2/authorize',
+//     'urlAccessToken'          => 'fusionauth:9011/oauth2/token',
+//     'urlResourceOwnerDetails' => 'fusionauth:9011/oauth2/userinfo',
+// ]);
 
-if (!isset($_GET['code'])) {
+// if (!isset($_GET['code'])) {
 
-    // If we don't have an authorization code then get one
-    $authUrl = $provider->getAuthorizationUrl();
-    $_SESSION['oauth2state'] = $provider->getState();
-    header('Location: '.$authUrl);
-    exit;
+//     // If we don't have an authorization code then get one
+//     $authUrl = $provider->getAuthorizationUrl();
+//     $_SESSION['oauth2state'] = $provider->getState();
+//     header('Location: '.$authUrl);
+//     exit;
 
-// Check given state against previously stored one to mitigate CSRF attack
-} elseif (empty($_GET['state']) || ($_GET['state'] !== $_SESSION['oauth2state'])) {
+// // Check given state against previously stored one to mitigate CSRF attack
+// } elseif (empty($_GET['state']) || ($_GET['state'] !== $_SESSION['oauth2state'])) {
 
-    unset($_SESSION['oauth2state']);
-    exit('Invalid state');
+//     unset($_SESSION['oauth2state']);
+//     exit('Invalid state');
 
-} else {
+// } else {
 
-    // Try to get an access token (using the authorization code grant)
-    $token = $provider->getAccessToken('authorization_code', [
-        'code' => $_GET['code']
-    ]);
+//     // Try to get an access token (using the authorization code grant)
+//     $token = $provider->getAccessToken('authorization_code', [
+//         'code' => $_GET['code']
+//     ]);
 
-    // Optional: Now you have a token you can look up a users profile data
-    try {
+//     // Optional: Now you have a token you can look up a users profile data
+//     try {
 
-        // We got an access token, let's now get the user's details
-        $user = $provider->getResourceOwner($token);
+//         // We got an access token, let's now get the user's details
+//         $user = $provider->getResourceOwner($token);
 
-        // Use these details to create a new profile
-        printf('Hello %s!', $user->getNickname());
+//         // Use these details to create a new profile
+//         printf('Hello %s!', $user->getNickname());
 
-    } catch (Exception $e) {
+//     } catch (Exception $e) {
 
-        // Failed to get user details
-        exit('Oh dear...');
-    }
+//         // Failed to get user details
+//         exit('Oh dear...');
+//     }
 
-    // Use this to interact with an API on the users behalf
-    echo $token->getToken();
-}
+//     // Use this to interact with an API on the users behalf
+//     echo $token->getToken();
+// }
