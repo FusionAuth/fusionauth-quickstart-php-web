b6d75a1104067aa71c824cb9e4ceb0873019118f
Richard JE Cooke <RichardJECooke@pm.me>
2023-11-17 09:13:11 +0200

    debugging jerryhopper object

--- your-application/src/auth.php
+++ your-application/src/auth.php
@@ -1,5 +1,10 @@
 <?php
 
+# display errors in server console, but don't send to browser
+// ini_set('display_errors', 0);
+// ini_set('log_errors', '1');
+// ini_set('error_log', 'php://stderr');
+
 # load all composer packages automatically. root folder is one level up.
 require_once __DIR__ . '/../vendor/autoload.php';
 
@@ -12,17 +17,22 @@ $fusionAuthClientSecret = $_ENV['FUSIONAUTH_CLIENT_SECRET'] ?? getenv('FUSIONAUT
 $fusionAuthBaseUrl =      $_ENV['FUSIONAUTH_BASE_URL']      ?? getenv('FUSIONAUTH_BASE_URL');
 $fusionAuthRedirectUrl =  $_ENV['FUSIONAUTH_REDIRECT_URL']  ?? getenv('FUSIONAUTH_REDIRECT_URL');
 
+// $options = [
+//     'state' => 'OPTIONAL_CUSTOM_CONFIGURED_STATE',
+//     'scope' => ['openid email profile']
+// ];
+// $authorizationUrl = $provider->getAuthorizationUrl($options);
+
 $provider = new \JerryHopper\OAuth2\Client\Provider\FusionAuth([
     'clientId'          => $fusionAuthClientId,
     'clientSecret'      => $fusionAuthClientSecret,
     'redirectUri'       => $fusionAuthRedirectUrl,
-    'urlAuthorize'            => $fusionAuthBaseUrl . '/oauth2/authorize', # 'http://host.docker.internal:9011/oauth2/authorize'
-    'urlAccessToken'          => $fusionAuthBaseUrl . '/oauth2/token',
-    'urlResourceOwnerDetails' => $fusionAuthBaseUrl . '/oauth2/userinfo',
+    'urlAuthorize'            => $fusionAuthBaseUrl . '/oauth2/authorize',
+    'urlAccessToken'          => 'http://host.docker.internal:9011' . '/oauth2/token',
+    'urlResourceOwnerDetails' => 'http://host.docker.internal:9011' . '/oauth2/userinfo',
 ]);
 
-// "Invalid redirect_uri http://localhost:9012/auth/callback",
-
+session_start();
 // send user to fusionauth login, if not coming from there
 if (!isset($_GET['code'])) {
     $authUrl = $provider->getAuthorizationUrl();
@@ -40,10 +50,24 @@ else {
     ]);
     try {
         $user = $provider->getResourceOwner($token);
-        printf('Hello %s!', $user->getNickname());
+        // var_dump($user); # see all properties returned by fusionauth
+        // "applicationId"=>"e9fdb985-9173-4e01-9d73-ac2d60d1dc8e", "birthdate"=>"1985-11-23", "email"=>"richard@example.com", "email_verified"=>true, "family_name"=>"Hendricks", "given_name"=>"Richard", "roles"=>{}, "sub"=>"00000000-0000-0000-0000-111111111111", "tid"=>"d7d09513-a3f5-401c-9685-34ab6c552453"
+
+        $methods = get_class_methods($user);
+        echo "<pre>";
+        print_r($methods);
+        echo "</pre>";
+
+        $userArray = $user->toArray();
+        // $email = $user->getEmail();
+        // $name = $user->getNickname();
+        $name = $userArray['given_name'];
+        printf('Hello %s!', $name);
     }
     catch (Exception $e) {
         exit('Failed to get user details from FusionAuth');
     }
     echo $token->getToken();
 }
+
+
